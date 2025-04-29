49ae508e185f22fd53578c35052cf04c61e5755a
bayazidsustami <bayazidsustamy@gmail.com>
2025-04-29 21:09:25 +0700

    feat : create user db migration

---
+++ db/migrations/20250429135846_create_users_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_users_username ON users(username);
+CREATE INDEX idx_users_email ON users(email);
